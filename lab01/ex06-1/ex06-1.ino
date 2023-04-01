702787cf1c1d334dfa46b354879295f2c060c03d
Hamrrick <Hamrrick@noreply.github.com>
2023-04-01 17:12:11 +0200

    Correzione es6

--- lab01/ex06-1/ex06-1.ino
+++ lab01/ex06-1/ex06-1.ino
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <LiquidCrystal_PCF8574.h>
 #include <Wire.h>
 
@@ -9,20 +10,24 @@ const double TK = 273.15;
 
 double r, t;
 
-LiquidCrystal_PCF8574 lcd(0x27);
+LiquidCrystal_PCF8574 lcd(0x20);
 
 char lcdDisplay[2][20];
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial);
   Serial.println("Lab 1.5 starting");
+  
+  while (!Serial);
   
   Wire.begin();
   Wire.beginTransmission(0x27);
 
-  lcd.begin(20, 4);
+  lcd.begin(16, 2);
   lcd.setBacklight(255);
+  lcd.home();
+  lcd.clear();
+  lcd.print("Temperature:");
 
   pinMode(TEMP_PIN, INPUT);
 }
@@ -34,11 +39,11 @@ void loop() {
   r = (1023.0 / v - 1.0) * (double)R1;
   t = 1.0 / ( (log(r / (double)R0) / (double)B) + (1.0 / ((double)T0 + TK))) - TK;
 
+
   lcd.home();
   lcd.clear();
-  lcd.setCursor(0,0);
 
-  sprintf(lcdbuf, "Temperature: %.2f\0", (float)t);
+  sprintf(lcdbuf, "Temperature:%.2f\0", (float)t);
   lcd.print(lcdbuf);
 
   Serial.print("Temperature: ");
