6b7ae808d076c35d4a01eddd73b364d3c010430d
fraimparato <fra.imparato20@gmail.com>
2023-04-24 18:47:05 +0200

    feat: implemented hw lab 02, except points 4 and 9

--- HW_lab02/l02/l02.ino
+++ HW_lab02/l02/l02.ino
@@ -15,23 +15,25 @@
 const int B = 4275, T0 = 25;
 const long R0 = 100000, R1 = 100000;
 const double TK = 273.15;
-double r, temperature;
+double v, r, temperature;
 
 uint8_t air_conditioning_intensity;
+float ac_percentage;
 float min_ac_absence = 20;
 float max_ac_absence = 25;
-float min_ac_presence = 20;
+float min_ac_presence = 25;//20;
 float max_ac_presence = 30;
 uint8_t ac_pwm_value(float temp, float min, float max);
 
-const uint8_t min_fan_speed = 0;
+const uint8_t min_fan_speed = 124;    // min pwm to turn on the fan
 const uint8_t max_fan_speed = 255;
 
 uint8_t heating_intensity;
-float min_h_absence = 10;
-float max_h_absence = 15;
-float min_h_presence = 10;
-float max_h_presence = 20;
+float ht_percentage;
+float min_h_absence = 20;//10;
+float max_h_absence = 25;//15;
+float min_h_presence = 25;//10;
+float max_h_presence = 30;//20;
 uint8_t h_pwm_value(float temp, float min, float max);
 
 const uint8_t min_led_intensity = 0;
@@ -40,20 +42,20 @@ const uint8_t max_led_intensity = 255;
 uint8_t presence;
 
 uint8_t pir_presence;
-const int pir_timeout = 30 * 60 * 1000;
+const int pir_timeout = 10 * 1000;      //30 * 60 * 1000;
 int pir_time;
 void pir_presence_isr();
 
 uint8_t sound_presence;
-const uint8_t n_sound_events = 0;
-const int sound_threshold = 0;
-const int sound_interval = 60 * 60 * 1000;
-volatile short sample_buffer[256];      // buffer to read sample into, each sample is 16-bit
+const uint8_t n_sound_events = 2;   // tbc
+const int sound_threshold = 100;    // tbc
+const int sound_interval = 20 * 1000;   //60 * 60 * 1000;
+short sample_buffer[256];      // buffer to read sample into, each sample is 16-bit
 void on_PDM_data();
 int n_samples_read;
 
 LiquidCrystal_PCF8574 lcd(0x20);
-char lcd_buffer[21][2];
+char lcd_buffer[2][21];
 void refresh_display();
 uint8_t display_state;
 
@@ -78,15 +80,20 @@ void setup() {
         while (1);
     }
 
+    Wire.begin();
+    Wire.beginTransmission(0x27);
     lcd.begin(16, 2);
     lcd.setBacklight(255);
 
     air_conditioning_intensity = 0;
     heating_intensity = 0;
 
-    pir_presence = 0;
-    sound_presence = 0;
     presence = 0;
+
+    pir_presence = 0;
+    
+    sound_presence = 0;
+    n_samples_presence = 0;
 
     display_state = 1;
     Scheduler.startLoop(refresh_display);
@@ -94,7 +101,7 @@ void setup() {
 
 
 void loop() {
-
+    v = (double) analogRead(TEMPERATURE_PIN);
     r = (1023.0 / v - 1.0) * (double)R1;
     temperature = 1.0 / ( (log(r / (double)R0) / (double)B) + (1.0 / ((double)T0 + TK))) - TK;
 
@@ -119,6 +126,46 @@ void loop() {
     }
     analogWrite(FAN_PIN, air_conditioning_intensity);
     analogWrite(RED_LED_PIN, heating_intensity);
+
+    if (Serial.available()) {
+        char a_h, M_m, p_a;
+        float value;
+        String str = Serial.readString();
+        str.trim();
+        if (sscanf(str.c_str(), "%c %c %c: %f", &a_h, &M_m, &p_a, &value) == 4) {
+            if (a_h == 'a') {
+                if (M_m == 'M') {
+                    if (p_a == 'p') {
+                        max_ac_presence = value;
+                    } else if (p_a == 'a') {
+                        max_ac_absence = value;
+                    }
+                } else if (M_m == 'm') {
+                    if (p_a == 'p') {
+                        min_ac_presence = value;
+                    } else if (p_a == 'a') {
+                        min_ac_absence = value;
+                    }
+                }
+            } else if (a_h == 'h') {
+                if (M_m == 'M') {
+                    if (p_a == 'p') {
+                        max_h_presence = value;
+                    } else if (p_a == 'a') {
+                        max_h_absence = value;
+                    }
+                } else if (M_m == 'm') {
+                    if (p_a == 'p') {
+                        min_h_presence = value;
+                    } else if (p_a == 'a') {
+                        min_h_absence = value;
+                    }
+                }
+            }
+        }
+    }
+    
+    delay(100);
 }
 
 
@@ -134,7 +181,7 @@ void on_PDM_data() {
     // read into the sample buffer
     PDM.read(sample_buffer, bytes_available);
     // 16-bit, 2 bytes per sample
-    n_samples_read = bytes_available / 2;
+    n_samples_read += bytes_available / 2;
 }
 
 
@@ -143,7 +190,8 @@ uint8_t ac_pwm_value(float temp, float min, float max) {
         return 0;
     }
 
-    return (uint16_t) ((temp - min) / (max - min) * (max_fan_speed - min_fan_speed) + min_fan_speed);
+    ac_percentage = (temp - min) / (max - min);
+    return (uint16_t) (ac_percentage * (max_fan_speed - min_fan_speed) + min_fan_speed);
 }
 
 
@@ -152,7 +200,8 @@ uint8_t h_pwm_value(float temp, float min, float max) {
         return 0;
     }
 
-    return (uint16_t) ((max - temp) / (max - min) * (max_led_intensity - min_led_intensity) + min_led_intensity);
+    ht_percentage = (max - temp) / (max - min);
+    return (uint16_t) (ht_percentage * (max_led_intensity - min_led_intensity) + min_led_intensity);
 }
 
 
@@ -162,8 +211,8 @@ void refresh_display() {
     lcd.clear();
 
     if (display_state) {
-        sprintf(lcd_buffer[0], "T:%.1lf Pres:%d", temperature, 0);
-        sprintf(lcd_buffer[1], "AC:%d\% HT:$%d\%", air_conditioning_intensity / 255 * 100, heating_intensity / 255 * 100);
+        sprintf(lcd_buffer[0], "T:%.1lf Pres:%d", temperature, presence);
+        sprintf(lcd_buffer[1], "AC:%d%% HT:%d%%", (int) (ac_percentage * 100), (int) (ht_percentage * 100));
     }
     else {
         if (presence) {
@@ -178,7 +227,10 @@ void refresh_display() {
 
     display_state = 1 - display_state;
 
-    lcd.print(lcd_buffer);
+    lcd.setCursor(0, 0);
+    lcd.print(lcd_buffer[0]);
+    lcd.setCursor(0, 1);
+    lcd.print(lcd_buffer[1]);
 
-    delay(5000);
+    delay(3000);
 }
