323944e35f7c534451b231bf20ae541aa625f7eb
fraimparato <fra.imparato20@gmail.com>
2023-05-18 15:05:48 +0200

    feat: started HW_lab03

---
+++ HW_lab03/ex01/ex01.ino
@@ -0,0 +1,71 @@
+#include <WiFiNINA.h>
+#include "arduino_secrets.h"
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+
+
+void printResponse(WiFiClient client, int code, String body) {
+    client.println("HTTP/1.1 " + String(code));
+
+    if (client == 200) {
+        client.println("Content-type: application/json; charset=utf-8");
+        client.println();
+        client.println(body);
+    } else {
+        client.println();
+    }
+}
+
+void process(WiFiClient client) {
+    String req_type = client.readStringUntil(' ');
+    req_type.trim();
+
+    String url = client.readStringUntil(' ');
+    url.trim();
+
+    if (url.startWith("/led/")) {
+        String led_val = url.substring(5);
+
+        Serial.print("[DEBUG] LED Value: ");
+        Serial.println(led_val);
+
+        if (led_val == "0" || led_val == "1") {
+            int int_val = led_val.toInt();
+            digitalWrtie(LED_PIN, int_val);
+            printResponse(client, 200, senMlEncode("led", int_val, ""));
+        }
+    }
+
+    return;
+}
+
+void setup() {
+    Serial.begin(9600);
+
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to SSID: ")
+        Serial.println(ssid);
+
+        status = WiFi.begin(ssid, pass);
+        delay(2000);
+    }
+
+    Serial.print("Connected with IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop() {
+    WiFiClient client = server.available();
+
+    if (client) {
+        process(client);
+        client.stop();
+    }
+
+    delay(50);
+}
