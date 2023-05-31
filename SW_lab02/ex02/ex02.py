71831d379579f9cf4db94461aa1c489ec3482b8e
fraimparato <fra.imparato20@gmail.com>
2023-05-31 18:06:19 +0200

    feat: fixed small bugs, added debug's prints

--- SW_lab02/ex02/ex02.py
+++ SW_lab02/ex02/ex02.py
@@ -7,19 +7,21 @@ def main():
     port = input("Insert the server port: ")
 
     while True:
-        sel = input("Insert the type (d / u) you are looking for, or use 'c' to exit: ")
-        if sel == 'c':
-            break
+        sel = input("Insert the type (d / u) you are looking for, or use 'x' to exit: ")
 
         if sel == 'd':
             type = "devices"
-        if sel == 'u':
+        elif sel == 'u':
             type = "users"
+        elif sel == 'x':
+            break
         else:
-            print("Input not valid. Use 'd' for devices and 'u' for users.")
+            print("Input not valid. Use 'd' for devices, 'u' for users, 'x' for exit.")
             continue
 
         sel = input("Would you like to retreive information about a specific ID? (y / n): ")
+
+        id = ""
 
         if sel == 'y':
             id = input("Insert the ID: ")
@@ -27,6 +29,8 @@ def main():
             print("Input not valid. Use 'y' to insert ID, 'n' to not to. Continuing as 'n' was pressed...")
 
         get_url = f"{ip_addr}:{port}/{type}/{id}"
+
+        print(f"[DEBUG] - Sending request to {get_url}")
         r = requests.get(get_url)
 
         print(f"Request status code: {r.status_code}")
