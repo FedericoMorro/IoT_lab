7ccea264ce74587c504b7a87bf5bfa5b755d913e
fraimparato <fra.imparato20@gmail.com>
2023-06-01 16:57:03 +0200

    feat: requests added in swlab02/ex02

--- SW_lab02/ex03/Device.py
+++ SW_lab02/ex03/Device.py
@@ -1,4 +1,8 @@
 import requests
+import json
+
+ip_addr = "127.0.0.1"
+port = 8080
 
 class Device():
     def __init__(
@@ -9,16 +13,39 @@ class Device():
         resource: list
     ):
         self.device_id = device_id
-        self.rest_endpoints.append(rest_endpoints)
-        self.mqtt_endpoints.append(mqtt_endpoints)
-        self.resources.append(resource)
+        self.rest_endpoints = rest_endpoints
+        self.mqtt_endpoints = mqtt_endpoints
+        self.resources = resource
+
+        self.publish()
 
         return
     
+
+    def _generate_payload(self) -> dict:
+        pl = {
+            "id": self.device_id,
+            "end_points": {
+                "REST": self.rest_endpoints,
+                "MQTT": self.mqtt_endpoints
+            },
+            "info": {
+                "resources": self.resources
+            }
+        }
+
+        return pl
+    
     
     def publish(self):
-        pass
+        payload = self._generate_payload()
+        requests.post(f"http://{ip_addr}:{port}/devices/subscription", data = json.dumps(payload))
+
+        return
 
 
     def update(self):
-        pass
+        payload = self._generate_payload()
+        requests.put(f"http://{ip_addr}:{port}/devices/refresh", data = json.dumps(payload))
+
+        return
